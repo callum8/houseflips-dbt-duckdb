f38e74c9b0170ceb7f9f4c4829d7306d211b412b
callum8 <callumhryan@live.co.uk>
2025-05-28 16:49:08 +0100

    add docker container

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,20 +4,40 @@ provider "aws" {
 
 
 resource "aws_s3_bucket" "bucket_name" {
-  bucket = "flipping-bucket-${var.env_name}"
+  bucket = "duckdb-dbt-flips-${var.env_name}"
 
   tags = {
-    Name        = "ExampleBucket"
+    Name        = "flips-${var.env_name}"
     Environment = var.env_name
   }
 }
 
-resource "aws_s3_object" "folder" {
+
+
+
+resource "aws_s3_object" "target_folder" {
   bucket = aws_s3_bucket.bucket_name.id
-  key    = "my-folder/" # The folder name (must end with a slash)
+  key    = "dbt/modelled_data/" # The folder name (must end with a slash)
 }
 
-resource "aws_s3_object" "folder" {
+resource "aws_s3_object" "source_folder" {
   bucket = aws_s3_bucket.bucket_name.id
-  key    = "my-folder/" # The folder name (must end with a slash)
+  key    = "dbt/source_data/" # The folder name (must end with a slash)
+}
+
+resource "aws_ecr_repository" "flips_ecr" {
+  name = "flips-ecr-${var.env_name}"
+
+}
+
+
+resource "null_resource" "docker_push" {
+  provisioner "local-exec" {
+    command = <<EOT
+      docker build -t houseflips-dbt .
+      aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 398320704774.dkr.ecr.eu-west-2.amazonaws.com
+      docker tag houseflips-dbt:latest 398320704774.dkr.ecr.eu-west-2.amazonaws.com/flips-ecr-dev:latest
+      docker push 398320704774.dkr.ecr.eu-west-2.amazonaws.com/flips-ecr-dev:latest
+    EOT
+  }
 }
